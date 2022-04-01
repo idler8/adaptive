4efaedf899b5fa5c96c688122929f26cacb17376
闲人 <code@idler8.com>
2022-04-01 18:19:16 +0800

    feat: 初始化项目

---
+++ index.js
@@ -0,0 +1,14 @@
+(function(window){
+    var doc = window.document;
+    var script = doc.currentScript
+    var design = script.getAttribute('design') * 1 || 750
+    var scale = script.getAttribute('scale') * 1 || 100
+    var vw = design / scale;
+    var html = doc.documentElement;
+    var resize = function () {
+        var width = Math.min(html.clientWidth || 375, 750)
+        html.style.fontSize = width / vw + 'px'
+    }
+    window.addEventListener("orientationchange" in window ? "orientationchange" : "resize", resize, false)
+    doc.addEventListener("DOMContentLoaded", resize, false)
+})(window)
