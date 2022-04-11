5f8478cc98f95617a8974f6d2ffc950491d90079
闲人 <code@idler8.com>
2022-04-11 12:38:48 +0800

    feat: 增加兼容

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 (function(window){
     var doc = window.document;
-    var script = doc.currentScript
+    var script = doc.currentScript || (function() {
+        var scripts = doc.getElementsByTagName('script');
+        return scripts[scripts.length - 1];
+    })();
     var design = script.getAttribute('design') * 1 || 750
     var scale = script.getAttribute('scale') * 1 || 100
     var vw = design / scale;
